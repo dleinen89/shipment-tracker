6ed2f2d8e04fc221a20316a2ed3bd10dc963841c
dleinen89 <152758894+dleinen89@users.noreply.github.com>
2024-11-15 16:06:35 +1000

    Updated bg color for contrast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import ShipmentTracker from '../components/ShipmentTracker';
+'use client'
+
+import ShipmentTracker from '@/components/ShipmentTracker';
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-4">
+    <main className="min-h-screen p-4 bg-slate-50">
       <ShipmentTracker />
     </main>
   );
